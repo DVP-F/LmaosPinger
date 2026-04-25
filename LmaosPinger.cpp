3344454d752b5be92e9ef6478fd101b4b02ad14c
DVP-F <carnx@duck.com>
2026-04-25 21:08:38 +0100

    might switch to a diff lang too tbh. rust or zig anyone?

--- LmaosPinger.cpp
+++ LmaosPinger.cpp
@@ -1,19 +1,19 @@
-// LmaosPinger.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <string>
 #include <cstdlib>
 
-void pingHost(const std::string& command) {
-    int result = system(command.c_str());
-    if (result == 0) {
-        std::cout << "Ping command executed successfully." << std::endl;
-    }
-    else {
-        std::cout << "Ping command failed with error code: " << result << std::endl;
-    }
-}
+// void pingHost(const std::string& command) {
+//     int result = system(command.c_str());
+//     if (result == 0) {
+//         std::cout << "Ping command executed successfully." << std::endl;
+//     }
+//     else {
+//         std::cout << "Ping command failed with error code: " << result << std::endl;
+//     }
+// }
+
+// Construct packets at socket level instead. Max customizability and max CVE coverage
+// I'm also banning any WIN32 api use. This will be usable on linux native too
 
 int main(int argc, char* argv[]) {
     std::string host;
@@ -44,26 +44,14 @@ int main(int argc, char* argv[]) {
 
     if (loop) {
         while (true) {
-            pingHost(command);
+            // pingHost(command);
             std::cout << "Press Ctrl+C to stop..." << std::endl;
         }
     }
     else {
-        pingHost(command);
+        // pingHost(command);
     }
 
     return 0;
 }
 
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
